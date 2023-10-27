d4d918690296cd3231ffa417416c773ad7776fb1
Martina Tapia <martina.tapia@mi.unc.edu.ar>
2023-10-27 13:24:23 -0300

    Cambios de nombre en algunas variables

--- spyctral/io.py
+++ spyctral/io.py
@@ -6,8 +6,9 @@ import numpy as np
 
 from . import core
 
-
-FISA_RX_SPECTRA_NAMES = re.compile(r"Index (?P<index>\d) = (?P<value>[^\n]+)")
+FISA_RX_VERSION = re.compile(
+    r"SPECTRUM ANALYZED WITH FISA v\.\s+(?P<value>[\d.][^\n]+)"
+)
 
 FISA_RX_DATE_AND_TIME = re.compile(
     r"Date (?P<date>\d{2}/\d{2}/\d{4}); time (?P<time>\d{2}:\d{2}:\d{2})"
@@ -21,12 +22,13 @@ FISA_RX_NORMALIZATION_POINT = re.compile(
     r"Normalization Point:\s+(?P<value>[\d.][^\n]+)"
 )
 
-FISA_RX_VERSION = re.compile(
-    r"SPECTRUM ANALYZED WITH FISA v\.\s+(?P<value>[\d.][^\n]+)"
-)
+FISA_RX_SPECTRA_NAMES = re.compile(r"Index (?P<index>\d) = (?P<value>[^\n]+)")
 
 
 def _process_header(lines):
+    """
+    This function processes header lines.
+    """
     src = "\n".join(lines)
 
     fisa_version = FISA_RX_VERSION.findall(src)[0]
@@ -35,46 +37,59 @@ def _process_header(lines):
     datetime = dateutil.parser.parse(f"{date} {time}")
 
     reddening = float(FISA_RX_REDDENING.findall(src)[0])
-    adopted_templated = FISA_RX_TEMPLATE.findall(src)[0]
+    adopted_template = FISA_RX_TEMPLATE.findall(src)[0]
     normalization_point = float(FISA_RX_NORMALIZATION_POINT.findall(src)[0])
 
-    names = {}
+    spectra_names = {}
     for match in FISA_RX_SPECTRA_NAMES.finditer(src):
         idx = int(match.group("index"))
         name = match.group("value")
-        names[idx] = name
-    names = tuple(name for _, name in sorted(names.items()))
+        spectra_names[idx] = name
+    spectra_names = tuple(name for _, name in sorted(spectra_names.items()))
 
     header = {
         "fisa_version": fisa_version,
-        "dateime": datetime,
+        "date_time": datetime,
         "reddening": reddening,
-        "adopted_templated": adopted_templated,
+        "adopted_template": adopted_template,
         "normalization_point": normalization_point,
-        "names": names,
+        "spectra_names": spectra_names,
     }
 
     return header
 
 
 def read_fisa(path_or_buffer):
-    header_lines, spectras_lines = [], []
-    current_spectra = []
+    """
+    This function reads FISA file.
+
+    Parameters
+    ----------
+    filename: "str"
+        Name of file
+
+    Return
+    ----------
+    out: SpectralSummary
+
+    """
+    header_lines, spectra_lines = [], []
+    current_spectrum = []
     with open(path_or_buffer, "r") as fp:
         for line in fp:
             if line.startswith(" #"):
                 header_lines.append(line.strip())
             elif line.strip():
                 lamb, flux = map(float, line.split())
-                current_spectra.append([lamb, flux])
-            elif current_spectra and not line.strip():
-                spectras_lines.append(np.array(current_spectra, dtype=float))
-                current_spectra = []
+                current_spectrum.append([lamb, flux])
+            elif current_spectrum and not line.strip():
+                spectra_lines.append(np.array(current_spectrum, dtype=float))
+                current_spectrum = []
 
-    if current_spectra:
-        spectras_lines.append(np.array(current_spectra, dtype=float))
-        del current_spectra
+    if current_spectrum:
+        spectra_lines.append(np.array(current_spectrum, dtype=float))
+        del current_spectrum
 
     header = _process_header(header_lines)
 
-    return core.SpectralSummary(None, extra=header)
+    return core.SpectralSummary(None, header=header)

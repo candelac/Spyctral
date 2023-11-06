9f3af257011e042778f6619bebfeec2c3936cd88
Jose Luis MARTINEZ <jlmartinez.academico@gmail.com>
2023-11-06 12:52:21 -0300

    Actualizacion de test_core.py en mi rama, agregue def test_type(self)

--- tests/test_core.py
+++ tests/test_core.py
@@ -6,12 +6,16 @@
 # All rights reserved.
 import os
 import sys
-import unittest
+# import unittest
+# import pytest
 from pathlib import Path
 
-sys.path.append("/home/joseluis/Proyectos/")
 
+sys.path.append("/home/joseluis/Proyectos/")
+from Spyctral.spyctral import core
 from Spyctral.spyctral import io
+
+from astropy.table import QTable
 
 
 PATH = Path(os.path.abspath(os.path.dirname(__file__)))
@@ -21,28 +25,42 @@ TEST_DATA_PATH = PATH / "Add_on"
 path = TEST_DATA_PATH / "fisa_1.fisa"
 
 ss = io.read_fisa(path)
-#print(ss.header)
+# print(ss.header)
 
-#print(len(ss.data.Unreddened_spectrum))
+print("tipo de objeto", type(ss))
 
 
-class Test_SpectralSummary(unittest.TestCase):
+class Test_SpectralSummary():  # (unittest.TestCase):
     def test_header(self):
         # Verificar que el campo 'header' sea igual al valor proporcionado
-        self.assertIn("fisa_version", ss.header)
-        self.assertIn("date_time", ss.header)
-        self.assertIn("spectra_names", ss.header)
-        self.assertIn("normalization_point", ss.header)
-        self.assertIn("adopted_template", ss.header)
-        self.assertIn("reddening", ss.header)
+        # self.assertIn("fisa_version", ss.header)
+        # self.assertIn("date_time", ss.header)
+        # self.assertIn("spectra_names", ss.header)
+        # self.assertIn("normalization_point", ss.header)
+        # self.assertIn("adopted_template", ss.header)
+        # self.assertIn("reddening", ss.header)
+        assert "fisa_version" in ss.header
+        assert "date_time" in ss.header
+        assert "spectra_names" in ss.header
+        assert "normalization_point" in ss.header
+        assert "adopted_template" in ss.header
+        assert "reddening" in ss.header
 
     def test_data(self):
-        self.assertEqual(len(ss.data.Unreddened_spectrum), 3000)
-        self.assertEqual(len(ss.data.Template_spectrum), 3401)
-        self.assertEqual(len(ss.data.Observed_spectrum), 3000)
-        self.assertEqual(len(ss.data.Residual_flux), 3000)
-       
+        # self.assertEqual(len(ss.data.Unreddened_spectrum), 3000)
+        # self.assertEqual(len(ss.data.Template_spectrum), 3401)
+        # self.assertEqual(len(ss.data.Observed_spectrum), 3000)
+        # self.assertEqual(len(ss.data.Residual_flux), 3000)
+        assert len(ss.data.Unreddened_spectrum) == 3000
+        assert len(ss.data.Template_spectrum) == 3401
+        assert len(ss.data.Observed_spectrum) == 3000
+        assert len(ss.data.Residual_flux) == 3000
 
-
-if __name__ == "__main__":
-    unittest.main()
+    def test_type(self):
+        assert isinstance(ss, core.SpectralSummary)
+        assert isinstance(ss.data.Unreddened_spectrum, QTable)
+        assert isinstance(ss.data.Template_spectrum, QTable)
+        assert isinstance(ss.data.Observed_spectrum, QTable)
+        assert isinstance(ss.data.Residual_flux, QTable)
+        assert any("SpectralSummary")
+        assert type(ss).__name__ == "SpectralSummary"

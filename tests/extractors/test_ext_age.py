6aa81f0d45ef2bc6c0c7d7e8507386c3153e97da
Martina Tapia <martina.tapia@mi.unc.edu.ar>
2023-11-11 12:58:33 -0300

    Creacion de extractors

---
+++ tests/extractors/test_ext_age.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# License: MIT
+# Copyright (c) 2023, Cerdosino Candela, Fiore J.Manuel, Martinez J.Luis,
+# Tapia-Reina Martina
+# All rights reserved.
+
+# =============================================================================
+# DOC
+# =============================================================================
+
+"""All spyctral.extractors tests"""

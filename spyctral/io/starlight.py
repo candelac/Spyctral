c60ae2569c47604d130c70b62cbd18a3174f66d4
Martina Tapia <martina.tapia@mi.unc.edu.ar>
2024-11-18 16:28:04 +0400

    Cambios en get_ssp_contributions

--- spyctral/io/starlight.py
+++ spyctral/io/starlight.py
@@ -226,9 +226,9 @@ def _get_ssp_contributions(tables_dict, xj_percent):
     # Convertir todas las columnas a números flotantes
     ssps_vector = ssps_vector.apply(pd.to_numeric, errors="coerce")
 
-    ssps_vector = ssps_vector[
-        ssps_vector["x_j"] > xj_percent
-    ].reset_index(drop=True)
+    ssps_vector = ssps_vector[ssps_vector["x_j"] > xj_percent].reset_index(
+        drop=True
+    )
     ssps_vector["x_j"] = (ssps_vector["x_j"] * 100) / ssps_vector["x_j"].sum()
 
     return ssps_vector
